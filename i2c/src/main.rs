e1d480fe928aa136bc3a0922c3ed6bb95e7ff449
roman shushakov <roman.a.shushakov1@gmail.com>
2024-02-05 21:53:02 +0100

    read accelerometer data

--- i2c/src/main.rs
+++ i2c/src/main.rs
@@ -7,18 +7,12 @@ use cortex_m_rt::entry;
 use panic_rtt_target as _;
 use rtt_target::{rtt_init_print, rprintln};
 
-use microbit::hal::prelude::*;
 use microbit::
 {
-    hal::twim, pac::twim0::frequency::FREQUENCY_A,
+    hal::{twim, Delay}, pac::twim0::frequency::FREQUENCY_A,
 };
 
-
-const ACCELEROMETER_ADDR: u8 = 0b0011001;
-const MAGNETOMETER_ADDR: u8 = 0b0011110;
-
-const ACCELEROMETER_ID_REG: u8 = 0x0f;
-const MAGNETOMETER_ID_REG: u8 = 0x4f;
+use lsm303agr::{AccelOutputDataRate, Lsm303agr, AccelMode};
 
 
 #[entry]
@@ -27,17 +21,20 @@ fn main() -> !
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
-    let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
 
-    let mut acc = [0];
-    let mut mag = [0];
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
 
-    // First write the address + register onto the bus, then read the chip's responses
-    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
-    i2c.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
-
-    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
-    rprintln!("The magnetometer chip's id is: {:#b}", mag[0]);
-
-    loop {}
+    let mut delay = Delay::new(board.SYST);
+    sensor.set_accel_mode_and_odr(&mut delay, AccelMode::Normal, AccelOutputDataRate::Hz50).unwrap();
+    loop 
+    {
+        if sensor.accel_status().unwrap().xyz_new_data()
+        {
+            let data = sensor.acceleration().unwrap();
+            // RTT instead of normal print
+            rprintln!("Acceleration: x {} y {} z {}", data.x_mg(), data.y_mg(), data.z_mg());
+        }
+    }
 }
